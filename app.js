e0152b37c0e4a3b84bcaf1a73ad3cb691f93529d
Omar <omarkhial74@gmail.com>
2022-03-01 19:28:50 +0100

    update cors policy

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
